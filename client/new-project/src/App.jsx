02b230da2acf686f235de390055b846657701b6a
Shivaprasadgunaganti <shivaprasad06559@gmail.com>
2025-02-18 14:55:13 +0530

    carousel

--- client/new-project/src/App.jsx
+++ client/new-project/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
 
 const App = () => {
     return (
         <div>
             <Navbar />
             {/* Your other content here */}
+            <Carousel/>
         </div>
     );
 };
