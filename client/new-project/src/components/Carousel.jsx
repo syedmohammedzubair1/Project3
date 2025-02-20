b518ad64ef779264a1ec93841b96ea990b3172df
Shivaprasadgunaganti <shivaprasad06559@gmail.com>
2025-02-20 10:11:10 +0530

    changes

--- client/new-project/src/components/Carousel.jsx
+++ client/new-project/src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
