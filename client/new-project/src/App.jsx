6d290ce60cba0c5d73a654c566b45d3d418f8b8c
Valluri Udaya Sri <udayasrivalluri354@gmail.com>
2025-02-24 10:29:55 +0530

    shivaprasad

--- client/new-project/src/App.jsx
+++ client/new-project/src/App.jsx
@@ -21,29 +21,41 @@
 // export default App;
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+// import { CartProvider } from "./context/CartContext";
 // import ItemList from "./pages/ItemList";
 // import { ItemList } from "./pages/ItemList";  // ✅ Correct way for named export
-import { ItemList } from "./data/ItemList";
+// import { ItemList } from "./data/ItemList";
 
-import CartPage from "./pages/CartPage";
-import Navbar from "./components/Navbar";
+// import CartPage from "./pages/CartPage";
+// import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HomeWebinar } from "./components/Webinar/HomeWebinar";
+import { Webinar } from "./components/Webinar/Webinar";
+import { Purchase } from "./components/Purchase/Purchase";
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Navbar />
-        <div className="container mt-4">
-          <Routes>
-            <Route path="/" element={<ItemList />} />
-            <Route path="/cart" element={<CartPage />} />
-          </Routes>
-        </div>
-      </Router>
-    </CartProvider>
+    // <CartProvider>
+    //   <Router>
+    //     <Navbar />
+    //     <div className="container mt-4">
+    //       <Routes>
+    //         <Route path="/" element={<ItemList />} />
+    //         <Route path="/cart" element={<CartPage />} />
+    //       </Routes>
+    //     </div>
+    //   </Router>
+    // </CartProvider>
+    <>
+    <Purchase/>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<HomeWebinar/>} />
+        <Route path="/webinar" element={<Webinar />} />
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
