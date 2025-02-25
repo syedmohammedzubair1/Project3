cefbdbca7b5673b2c019bad2c93544689a248d25
Zubair <95565992+syedmohammedzubair1@users.noreply.github.com>
2025-02-25 16:24:49 +0530

    Revert "Shivaprasad"

--- client/new-project/src/App.jsx
+++ client/new-project/src/App.jsx
@@ -1,17 +1,54 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar } from "react-bootstrap";
-import Routing from "./components/Routing";
+import React, { useEffect } from "react";
+import Navbar from './components/Navbar';
 import Footer from "./components/Footer/Footer";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+// import "bootstrap-icons/font/bootstrap-icons.css";
+import { useLocation } from "react-router-dom";
+import Routing from "./components/Routing";
+import NicheFlare from "./components/NicheFlare";
+import SubscribePanel from "./components/SubscribePanel";
+import { CartProvider } from "./context/CartContext";
+import { HomeWebinar } from "./components/Webinar/HomeWebinar";
+import { Webinar } from "./components/Webinar/Webinar";
+import { Purchase } from "./components/Purchase/Purchase";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
+import Carousel2 from "./components/Carousel2";
+
+import Routing from "./components/Routing";
+import Carousel from "./components/Carousel";
+import React from 'react';
+// import { Webinar } from './components/Webinar/Webinar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { HomeWebinar } from './components/Webinar/HomeWebinar';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
+
+  const location = useLocation();
+  const isSubscribePanel = location.pathname.startsWith("/subscribe");
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div>
-      <Navbar />
+    <div className="App">
+      {/* <header>
+        <Navbar />
+      </header> */}
+        {/* Show NicheFlare navbar for Subscribe Panel, otherwise show the default Navbar */}
+      {isSubscribePanel ? <NicheFlare /> : <header><Navbar /></header>}
+
       <Routing />
-      <Footer />
-    </div>
-  );
+
+      {/* Hide Footer on Subscribe Panel */}
+      {!isSubscribePanel && <Footer />}
+            <h2>welcome</h2>
+        </div>
+    );
+
 };
 
 export default App;
